d609497576530d28238e9e28edb48c91e95e706f
bossniger <egorbelyakovich2@gmail.com>
2024-07-25 15:04:46 +0300

    lesson19 is done

--- Lesson_19/main.py
+++ Lesson_19/main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
+# Получаем айдишник последнего добавленного продукта в список
 def get_last_id():
     with open('products.json') as file:
         data = json.load(file)
@@ -11,12 +12,33 @@ def get_last_id():
     return last_id
 
 
+# Добавляем новый продукт в список
 def add_product_to_json(new_product):
     with open('products.json', 'r') as file:
         data = json.load(file)
     data['products'].append(new_product)
     with open('products.json', 'w') as file:
         json.dump(data, file, indent=4)
+
+
+# Добавление продукта в список потребленных продуктов
+def add_ate_product(product_id):
+    with open('products.json', 'r') as file:
+        products_list = json.load(file)
+
+    ate_product = None
+    for product in products_list['products']:
+        if int(product['id']) == int(product_id):
+            ate_product = product
+
+    if ate_product:
+        with open('eated_products.json', 'r') as file:
+            ate_products_list = json.load(file)
+        ate_products_list['ate_products'].append(ate_product)
+        with open('eated_products.json', 'w') as file:
+            json.dump(ate_products_list, file, indent=4)
+    else:
+        print("Product is not found")
 
 
 @app.route('/', methods=['GET'])
@@ -53,21 +75,22 @@ def create_product():
     return render_template('new_product.html')
 
 
+@app.route('/products/<int:product_id>')
+def update_ate_products(product_id):
+    add_ate_product(product_id)
+    return render_template('index.html')
+
+
+@app.route('/myfood')
+def show_my_food():
+    with open('eated_products.json', 'r') as file:
+        calories_sum = 0
+        data = json.load(file)
+        ate_products = data['ate_products']
+        for d in data['ate_products']:
+            calories_sum += d['calories']
+    return render_template('my_food.html', ate_products=ate_products, calories_sum=calories_sum)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-# # Открываем JSON файл для чтения
-# with open('products.json', 'r') as file:
-#     data = json.load(file)
-#
-# new_product = {
-#     'name':"igor",
-#     'age':3,
-#     'email':"3@gmail.com"
-# }
-# data['products'].append(new_product)
-#
-# # Записываем обновленные данные в JSON файл
-# with open('products.json', 'w') as file:
-#     json.dump(data, file, indent=4)
-#
-# print("Данные успешно добавлены в JSON файл.")

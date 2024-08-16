09625eb0513467a34ebf57eefb866309e5963f8c
bossniger <egorbelyakovich2@gmail.com>
2024-08-16 13:43:36 +0300

    Создать проект Blog с тремя приложениями: Article Author Comment Создать модели в каждом из них, создать миграции и запустить их. При этом модели должны быть связаны

---
+++ Lesson_22/dz_22/blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+
+# Create your models here.
